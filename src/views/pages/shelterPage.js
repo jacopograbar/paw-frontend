e2e2018411f4061bcbacaee19a8a7f9a215bed56
Jacopo G <S3876518@student.rmit.edu.au>
2023-11-14 11:36:47 +1100

    Shelter first section

--- src/views/pages/shelterPage.js
+++ src/views/pages/shelterPage.js
@@ -1,33 +1,65 @@
-import App from '../../App'
-import {html, render } from 'lit-html'
-import {gotoRoute, anchorRoute} from '../../Router'
-import Auth from '../../Auth'
-import Utils from '../../Utils'
+import App from "../../App";
+import { html, render } from "lit-html";
+import { gotoRoute, anchorRoute } from "../../Router";
+import Auth from "../../Auth";
+import Utils from "../../Utils";
 
 class ShelterPageView {
-  init(){
-    document.title = 'Shelter Page'    
-    this.render()    
-    Utils.pageIntroAnim()
+  init() {
+    document.title = "Shelter Page";
+    const shelterID = window.location.pathname.split("/")[2];
+    console.log("Shelter page for ID ", shelterID);
+    this.shelters = Utils.createDummyShelterObjects();
+    this.shelter = this.shelters[0];
+    this.render();
+    Utils.pageIntroAnim();
   }
 
-  render(){
+  render() {
     const template = html`
       <paw-header title="Shelter Page"></paw-header>
       <div class="shelter-page-view layout-page">
-      <div class="page-animation"></div>
+        <div class="page-animation"></div>
         <!-- First Section -->
         <section class="shelter-page-section layout-section">
-        <img id="first-background-img" src="../images/pawprint-white.png" />
+          <div class="first-column">
+            <sl-avatar class="avatar-main" label="${this.shelter.name}">
+              <img slot="icon" src="${this.shelter.profilePic}" />
+            </sl-avatar>
+            <h1>${this.shelter.name}</h1>
+            <p id="shelter-address">
+              <img
+                src="../images/location-pin.png"
+                id="location-pin"
+                alt="Location Pin"
+              />
+              ${this.shelter.address}, ${this.shelter.state}
+            </p>
+          </div>
+          <div class="second-column">
+            <h2>About the shelter</h2>
+            <p>${this.shelter.bio}</p>
+            <h2>Available pets</h2>
+            <div class="badges-row">
+              ${this.shelter.animals.map(
+                (animal) => html` <sl-avatar
+                  class="avatar-badge"
+                  label="${animal}"
+                >
+                  <img slot="icon" src="${animal == "Cats" ? "../images/cat-white.png" : "../images/dog-white.png" }" />
+                </sl-avatar>`
+              )}
+            </div>
+          </div>
+          <img id="first-background-img" src="../images/pawprint-white.png" />
         </section>
         <section class="shelter-page-section layout-section">
-        <img id="first-background-img" src="../images/pawprint-white.png" />
+          <img id="first-background-img" src="../images/pawprint-white.png" />
         </section>
-      </div>    
-    `
-    render(template, App.rootEl)
+      </div>
+    `;
+    render(template, App.rootEl);
   }
 }
 
-
-export default new ShelterPageView()
+export default new ShelterPageView();
